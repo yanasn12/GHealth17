cc3287e974a9436107b7c353abc9ad75955059db
guy berger <g.berger10@gmail.com>
2016-05-24 16:18:41 +0300

    24/5/16

--- G3/OCSF_PRO/src/javagui/views/Main.java
+++ G3/OCSF_PRO/src/javagui/views/Main.java
@@ -115,42 +115,42 @@ public class Main extends JFrame {
 				if(data.isEmpty())
 				{
 						JOptionPane.showMessageDialog(null,"you have enter a wrong Username.", "Login Error",JOptionPane.ERROR_MESSAGE);
-						
+						wrongConter=0;
 				}
 				else
 				{
-				dataDB=data.get(0).split(",");
-				if(!dataDB[4].equals("0"))
-				{
-					if(dataDB[4].equals("1"))
-						JOptionPane.showMessageDialog(null,"The user is already in Login mode,\n please connect to system administrator ", "Login Error",JOptionPane.ERROR_MESSAGE);
+					dataDB=data.get(0).split(",");
+					if(!dataDB[4].equals("0"))
+					{
+						if(dataDB[4].equals("1"))
+							JOptionPane.showMessageDialog(null,"The user is already in Login mode,\n please connect to system administrator ", "Login Error",JOptionPane.ERROR_MESSAGE);
+						else
+							if(dataDB[4].equals("2"))
+						JOptionPane.showMessageDialog(null,"The user is locked out of the system due to suspicion of hacking attempt,\n please connect to system administrator ", "Login Error",JOptionPane.ERROR_MESSAGE);
+					}
 					else
-						if(dataDB[4].equals("2"))
-					JOptionPane.showMessageDialog(null,"The user is locked out of the system due to suspicion of hacking attempt,\n please connect to system administrator ", "Login Error",JOptionPane.ERROR_MESSAGE);
-
-				}
-				else
-				{
-					
-					if(!dataDB[2].equals(txtPassword.getText().toString()))
-						{
-							System.out.println(wrongConter++);
-							if(wrongConter<3)
-								JOptionPane.showMessageDialog(null,"you have enter a wrong password.", "Login Error",JOptionPane.ERROR_MESSAGE);
-							else
-								{
-									inputLogin="update:medical_worker:"+"worker_num,"+ dataDB[1].toString()+",is_connected,2";
-									jdbc.mysqlConnection.ActionMode(inputLogin.toString());
-									JOptionPane.showMessageDialog(null,"you have try too many time, please connect your Admin.", "Login Error",JOptionPane.ERROR_MESSAGE);
-								}
-							
-						}
-					else if(dataDB[2].equals(txtPassword.getText().toString()))
-						{
-						inputLogin="update:medical_worker:"+"worker_num,"+ dataDB[0]+",is_connected,1";
-						jdbc.mysqlConnection.ActionMode(inputLogin.toString());
-						}
-				}
+					{
+						
+						if(!dataDB[2].equals(txtPassword.getText().toString()))
+							{
+								System.out.println(wrongConter++);
+								if(wrongConter<3)
+									JOptionPane.showMessageDialog(null,"you have enter a wrong password.", "Login Error",JOptionPane.ERROR_MESSAGE);
+								else
+									{
+										inputLogin="update:medical_worker:"+"worker_num,"+ dataDB[1].toString()+",is_connected,2";
+										jdbc.mysqlConnection.ActionMode(inputLogin.toString());
+										JOptionPane.showMessageDialog(null,"you have try too many time, please connect your Admin.", "Login Error",JOptionPane.ERROR_MESSAGE);
+									}
+								
+							}
+						else if(dataDB[2].equals(txtPassword.getText().toString()))
+							{
+							inputLogin="update:medical_worker:"+"worker_num,"+ dataDB[1]+",is_connected,1";
+							System.out.println(inputLogin);
+							jdbc.mysqlConnection.ActionMode(inputLogin.toString());
+							}
+					}
 				inputLogin="";
 				}
 			}
